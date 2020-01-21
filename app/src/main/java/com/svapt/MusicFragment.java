1dfd780f6d16b8aaf85a692d706ba5bcf8aeb0f6
Jona <jona2255@gmail.com>
2020-01-21 19:08:46 +0100

    Reproductor

--- app/src/main/java/com/svapt/MusicFragment.java
+++ app/src/main/java/com/svapt/MusicFragment.java
@@ -3,6 +3,9 @@ package com.svapt;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +14,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.google.android.material.tabs.TabLayout;
 import com.svapt.ui.library.PlaylistsFragment;
--- app/src/main/java/com/svapt/ui/library/SongsFragment.java
+++ app/src/main/java/com/svapt/ui/library/SongsFragment.java
@@ -2,6 +2,11 @@ package com.svapt.ui.library;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,16 +14,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
-
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.svapt.Cancion;
@@ -92,7 +90,7 @@ public class SongsFragment extends Fragment {
                 @Override
                 public void onClick(View view) {
                     mainViewModel.establecerCancionSeleccionada(cancion);
-                    navController.navigate(R.id.inicioFragment);
+                    navController.navigate(R.id.parent_view);
                 }
             });
         }
