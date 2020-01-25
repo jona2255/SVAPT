90445ebe28d9d47b4fb29812a990b68eb9331c06
Jona <jona2255@gmail.com>
2020-01-25 12:24:59 +0100

    Playlists

---
+++ app/src/main/java/com/svapt/Playlist.java
@@ -0,0 +1,18 @@
+package com.svapt;
+
+public class Playlist {
+
+    private int image;
+    private String title;
+    public Playlist(int image, String title) {
+        this.image = image;
+        this.title = title;
+    }
+    public int getImage() {
+        return image;
+    }
+    public String getTitle() {
+        return title;
+    }
+}
+
